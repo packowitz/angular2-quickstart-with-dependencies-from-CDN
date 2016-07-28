c2333ee2bb146cb92ee88a8bc609c9f81f03578a
Philipp Schumacher <p.schumacher@auckland.ac.nz>
2016-07-28 15:23:02 +1200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# angular2-quickstart-with-dependencies-from-CDN
-Showing an easy setup to serve the 3rd party libs (angular2 and its dependencies) from a CDN without tons of configuration and tools
+# Angular2 quickstart with dependencies loaded from a CDN
+Angular2 is nearly in final version available. So it is time to write awesome angular2 apps with typescript. The quickstart gives a good entry point to get started.
+After a while of programming you'll come to a point where you want to show your app to the world and let it run on a server and then you realize that all the dependencies are loaded from your local server what is definitely something to avoid.
+For loading 3rd party libraries it is recommended to load them from a common CDN.
+So I tried to find an easy solution to do so but I got screwed up very fast and nearly all found solution suggested the usage of tons of build tools like webpack, gulp, browserify and a lot more and combinations of them.
+When I start to write an angular2 application I don't want to spent hours or days in setting up a toolchain for building my frontend.
+After trying out a lof of tools I finally ended up in using exactly what is told in angulars quickstart guide with only a few changes what made me happy.
+
+This are the changes I made:
+* index.html
+replace the src path to shim, zone, Reflect and SystemJS from /node_modules/... to https://npmcdn.com/...
+
+* systemjs.config.js
+make the dependencies load as bundle from https://npmcdn.com/ too.
+Instead of '@angular': 'node_modules/@angular' you have to name all modules from angular and give the https://npmcdn.com/... path
+
+* tsconfig.json
+In case to avoid your own .ts/.js files loaded one by one you should bundle them. Typescript can do that without the need of any other bundline/build tool.
+"outFile": "./dist/bundle.js"
+But the output needs to be amd or system. As the quickstart guide makes already use of SystemJS, system may be the preferred way.
+"module": "system"
+Doing this means you have to load you app in the index.html a little bit different.
+
+System.import('dist/bundle.js').then(function() {
+    System.import('main').catch(function(err){ console.error(err); });
+});
+
+is doing the magic.
---
+++ systemjs.config.js
@@ -0,0 +1,57 @@
+/**
+ * System configuration for Angular 2 samples
+ * Adjust as necessary for your application needs.
+ */
+(function(global) {
+    // map tells the System loader where to look for things
+    var map = {
+        'app': 'app',
+        '@angular/common': 'https://npmcdn.com/@angular/common@2.0.0-rc.4',
+        '@angular/compiler': 'https://npmcdn.com/@angular/compiler@2.0.0-rc.4',
+        '@angular/core': 'https://npmcdn.com/@angular/core@2.0.0-rc.4',
+        '@angular/forms': 'https://npmcdn.com/@angular/forms@0.2.0',
+        '@angular/http': 'https://npmcdn.com/@angular/http@2.0.0-rc.4',
+        '@angular/platform-browser': 'https://npmcdn.com/@angular/platform-browser@2.0.0-rc.4',
+        '@angular/platform-browser-dynamic': 'https://npmcdn.com/@angular/platform-browser-dynamic@2.0.0-rc.4',
+        '@angular/router': 'https://npmcdn.com/@angular/router@3.0.0-beta.1',
+        '@angular/router-deprecated': 'https://npmcdn.com/@angular/router-deprecated@2.0.0-rc.2',
+        '@angular/upgrade': 'https://npmcdn.com/@angular/upgrade@2.0.0-rc.4',
+        'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
+        'rxjs': 'https://npmcdn.com/rxjs@5.0.0-beta.6'
+    };
+    // packages tells the System loader how to load when no filename and/or no extension
+    var packages = {
+        'app': { main: 'main.js', defaultExtension: 'js' },
+        'rxjs': { main: 'bundles/Rx.umd.js', defaultExtension: 'js' },
+        'angular2-in-memory-web-api': { main: 'index.js', defaultExtension: 'js' }
+    };
+    var ngPackageNames = [
+        'common',
+        'compiler',
+        'core',
+        'forms',
+        'http',
+        'platform-browser',
+        'platform-browser-dynamic',
+        'router',
+        'router-deprecated',
+        'upgrade'
+    ];
+    // Individual files (~300 requests):
+    function packIndex(pkgName) {
+        packages['@angular/'+pkgName] = { main: 'index.js', defaultExtension: 'js' };
+    }
+    // Bundled (~40 requests):
+    function packUmd(pkgName) {
+        packages['@angular/'+pkgName] = { main: 'bundles/' + pkgName + '.umd.js', defaultExtension: 'js' };
+    }
+    // Most environments should use UMD; some (Karma) need the individual index files
+    var setPackageConfig = System.packageWithIndex ? packIndex : packUmd;
+    // Add package entries for angular packages
+    ngPackageNames.forEach(setPackageConfig);
+    var config = {
+        map: map,
+        packages: packages
+    };
+    System.config(config);
+})(this);
